7a0a6b6ca45d7d5466b2ef6dd096fc5f905e3dfe
Min Thu Khant <minthukhant.mtk03@gmail.com>
2023-03-18 03:16:44 +0630

    auth context and protected

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,24 @@
-import React, { useEffect, useState } from "react"
-import "./App.css"
-import Task, { item } from "./components/task"
-import Input from "./components/Input"
+import React, { useContext } from "react"
+import AuthContextProvider, { AuthContext } from "./context/authContext"
+import './App.css'
+import ToDo from "./pages/Home"
+import About from "./pages/About"
+import Login from "./pages/login"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 
 function App() {
-
-
-  const [tasks, setTasks] = useState<Array<item>>([
-    { id: 1, desc: "go to gym" },
-    { id: 2, desc: "go home" },
-  ])
-
-  useEffect(()=> {
-    console.log("useeffect here")
-  }, [1])
-
-  const [keyword, setKeyword] = useState<string>("")
-
-  const addItem = () => {
-    setTasks([...tasks, { id: tasks.length + 1, desc: keyword }])
-    setKeyword("")
-  }
-
-  const deleteItem = (id: number) => {
-    const updatedItem = tasks.filter((item) => item.id !== id)
-    setTasks(updatedItem)
-  }
-
-  const updateItem = (value: item) => {
-    const updateItem = tasks.map((item) => {
-      if (value.id == item.id) {
-        return value
-      } else {
-        return item
-      }
-    })
-
-    setTasks(updateItem)
-  }
-
   return (
     <div className="App">
-      <div className="pj-wrap">
-        <div className="title">
-          <h1>What's the Plan Today?</h1>
-        </div>
-        <div className="todo-body">
-          <div className="todo">
-            <Input
-              placeholder={"add to do"}
-              value={keyword}
-              onChange={(e: {
-                target: { value: React.SetStateAction<string> }
-              }) => {
-                setKeyword(e.target.value)
-              }}
-            />
-            <button onClick={addItem}>ADD</button>
-          </div>
-
-          <div className="tasks">
-            {tasks.map((item, index) => {
-              return (
-                <Task
-                  key={index}
-                  item={item}
-                  handleDelete={deleteItem}
-                  handleUpdate={updateItem}
-                />
-              )
-            })}
-          </div>
-        </div>
-      </div>
+      <Router>
+        <AuthContextProvider>
+          <Routes>
+            <Route path="/" element={<ToDo />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </AuthContextProvider>
+      </Router>
     </div>
   )
 }
--- src/components/task/index.tsx
+++ src/components/task/index.tsx
@@ -1,11 +1,10 @@
-import React, { useState } from "react"
+import React, { useContext, useState } from "react"
 import styles from "./style.module.css"
 import Input from '../Input'
+import toDoContext from "../../context/toDoContext"
 
 type Props = {
   item: item
-  handleDelete: any
-  handleUpdate: any
 }
 
 export type item = {
@@ -13,13 +12,13 @@ export type item = {
   id: number
 }
 
-const Task: React.FC<Props> = ({ item, handleDelete, handleUpdate }) => {
-  const [input , setInput] = useState<string>("")
-  const [editMode , setEditMode] = useState<boolean>(false)
-
+const Task: React.FC<Props> = ({ item }) => {
+  const [input, setInput] = useState<string>("")
+  const [editMode, setEditMode] = useState<boolean>(false)
+  const { deleteItem, updateItem } = useContext(toDoContext)
   const updateValue = () => {
-    if(editMode){
-      handleUpdate({id: item.id, desc: input})
+    if (editMode) {
+      updateItem({ id: item.id, desc: input })
     }
     setEditMode(!editMode)
   }
@@ -27,15 +26,15 @@ const Task: React.FC<Props> = ({ item, handleDelete, handleUpdate }) => {
     <div className={styles.task}>
       <p>{item?.id + "." + item?.desc}</p>
       {
-        editMode && <Input placeholder={item.desc} className={styles.position} onChange={(e: { target: { value: React.SetStateAction<string> } })=> {setInput(e.target.value)}}  />
+        editMode && <Input placeholder={item.desc} className={styles.position} onChange={(e: { target: { value: React.SetStateAction<string> } }) => { setInput(e.target.value) }} />
       }
-      
+
       <div className="link">
         <i
           className={`fa-solid fa-trash ${styles.mycolor}`}
-          onClick={() => handleDelete(item.id)}
+          onClick={() => deleteItem(item.id)}
         ></i>
-        <i className={`fa-solid fa-pen-to-square ${styles.mycolor}`} 
+        <i className={`fa-solid fa-pen-to-square ${styles.mycolor}`}
           onClick={updateValue}
         ></i>
       </div>
